9013198b4f96f73d6b3a6697c257c452923e153d
hrishikesh kalekinge <100271790+HHrisHikesHH@users.noreply.github.com>
2022-05-29 13:50:41 +0530

    Add files via upload

---
+++ May/week 4/test 5/que3/square.js
@@ -0,0 +1,7 @@
+// Question 3. Check if for a given length and breadth, is the shape a square or rectangle. (eg: Let's say the length is 10 and breadth is 20, and then check).
+
+let l=10,b=20; //decla. and init.
+console.log(`lenth is ${l} and breath is ${b}`);
+// let res=l===b?"sqaure":"rectangle"; //using ternary operator.
+//console.log(res);
+console.log(l===b?"sqaure":"rectangle");//directly displaying without saving.
