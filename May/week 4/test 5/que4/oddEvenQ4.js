93719db51fdd6d998c2af7227c4b768bec16c9b2
hrishikesh kalekinge <100271790+HHrisHikesHH@users.noreply.github.com>
2022-05-29 13:52:26 +0530

    Add files via upload

---
+++ May/week 4/test 5/que4/oddEvenQ4.js
@@ -0,0 +1,19 @@
+// Question 4. Check if a number is odd or even using if-else statement.
+
+let no1=10,no2=11; //decl. init.
+if(no1%2==0) //division by two
+{
+    console.log(`the given no ${no1} is even`); //o/p
+}
+else
+{
+    console.log(`the given no ${no1} is odd`);
+}
+if(no2%2==0)
+{
+    console.log(`the given no ${no2} is even`);
+}
+else
+{
+    console.log(`the given no ${no2} is odd`);
+}
